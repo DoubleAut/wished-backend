7c56cb0df891a0f542aaa11cb3a1c6e7b9e148bd
RichardPickman <drezzerock@gmail.com>
2024-04-13 20:36:47 +0200

    add test case for controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -6,7 +6,36 @@ import { UsersService } from './users.service';
 describe('UsersController', () => {
     let usersController: UsersController;
 
-    const mockUsersService = {};
+    const mockUsersService = {
+        create: jest.fn((dto) => ({
+            id: 1,
+            email: 'mail@mail.com',
+            password: '123Qweasd',
+            name: 'Richard',
+            surname: 'Pickman',
+            picture: null,
+            isActive: false,
+        })),
+        update: jest.fn((dto) => ({
+            id: 1,
+            email: 'mail@mail.com',
+            password: '123Qweasd',
+            name: 'Alex',
+            surname: 'Pickman',
+            picture: null,
+            isActive: false,
+        })),
+        getPublicUserById: jest.fn((dto) => ({
+            id: 1,
+            email: 'mail@mail.com',
+            password: '123Qweasd',
+            name: 'Richard',
+            surname: 'Pickman',
+            picture: null,
+            isActive: false,
+        })),
+        remove: jest.fn((dto) => 1),
+    };
 
     beforeEach(async () => {
         const moduleRef = await Test.createTestingModule({
@@ -26,7 +55,62 @@ describe('UsersController', () => {
         usersController = moduleRef.get<UsersController>(UsersController);
     });
 
-    it('should return an array of cats', async () => {
-        expect(usersController).toBeDefined();
+    describe('UsersControllerCreate', () => {
+        it('Should create a user', async () => {
+            const user = {
+                email: 'mail@mail.com',
+                password: '123Qweasd',
+                name: 'Richard',
+                surname: 'Pickman',
+            };
+
+            expect(
+                await usersController.create(
+                    user.email,
+                    user.password,
+                    user.name,
+                    user.surname,
+                ),
+            ).toEqual({
+                ...user,
+                id: expect.any(Number),
+                picture: null,
+                isActive: false,
+            });
+        });
+
+        it('Should return an updated user', async () => {
+            const user = {
+                name: 'Alex',
+            };
+
+            expect(await usersController.update(1, user)).toEqual({
+                id: expect.any(Number),
+                email: 'mail@mail.com',
+                password: '123Qweasd',
+                name: 'Alex',
+                surname: 'Pickman',
+                picture: null,
+                isActive: false,
+            });
+        });
+
+        it('Should return a user by ID', async () => {
+            const user = {
+                id: 1,
+                email: 'mail@mail.com',
+                password: '123Qweasd',
+                name: 'Richard',
+                surname: 'Pickman',
+                picture: null,
+                isActive: false,
+            };
+
+            expect(await usersController.findOne(1)).toEqual(user);
+        });
+
+        it('Should return a amount of removed entities', async () => {
+            expect(await usersController.remove(1)).toEqual(1);
+        });
     });
 });
