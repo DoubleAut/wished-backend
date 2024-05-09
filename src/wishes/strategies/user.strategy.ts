9ce15e4a9033bd6b9167ea86891e2d4b86f05577
RichardPickman <drezzerock@gmail.com>
2024-05-09 10:42:09 +0200

    small refactor

--- src/wishes/strategies/user.strategy.ts
+++ src/wishes/strategies/user.strategy.ts
@@ -1,11 +1,12 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 /**
- * Validation guard - checks the decoded JWT token and userId provided in the body, to verificate the identity that made a request
+ * Validation guard - checks the identity of decoded JWT token and userId provided in the body
  */
 @Injectable()
 export class ValidationGuard implements CanActivate {
     constructor() {}
+
     canActivate(context: ExecutionContext): boolean {
         const request = context.switchToHttp().getRequest();
 
