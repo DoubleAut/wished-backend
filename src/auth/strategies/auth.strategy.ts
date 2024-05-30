0c0726282f4d13b69490aba45fab29bb3af69ccd
RichardPickman <drezzerock@gmail.com>
2024-05-30 16:34:01 +0200

    add flag local to auth strategy

--- src/auth/strategies/auth.strategy.ts
+++ src/auth/strategies/auth.strategy.ts
@@ -4,7 +4,7 @@ import { Strategy } from 'passport-local';
 import { UsersService } from '../../users/users.service';
 
 @Injectable()
-export class AuthStrategy extends PassportStrategy(Strategy) {
+export class AuthStrategy extends PassportStrategy(Strategy, 'local') {
     constructor(private usersService: UsersService) {
         super({ usernameField: 'email' });
     }
