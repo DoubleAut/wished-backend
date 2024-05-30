36f24717d3ba53707ed13eeb6614dc4f2ca7fc94
RichardPickman <drezzerock@gmail.com>
2024-05-30 16:49:30 +0200

    add CORS, CookieParser, UploadThing needed app uses

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import { createRouteHandler } from 'uploadthing/express';
 import { AppModule } from './app.module';
+import { uploadRouter } from './uploadthing';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe());
+    app.enableCors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    });
+
+    app.use(cookieParser());
+
+    app.use(
+        '/uploadthing',
+        createRouteHandler({
+            router: uploadRouter,
+        }),
+    );
 
     const configService = app.get(ConfigService);
     const port = configService.get<string>('API_PORT');
